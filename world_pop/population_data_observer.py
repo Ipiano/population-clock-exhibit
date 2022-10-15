53557ca03e4368254e6572cdd1b214f451e04765
Andrew Stelter <andrew.d.stelter@gmail.com>
2022-10-15 10:30:31 -0500

    Add observer pattern for population data

---
+++ world_pop/population_data_observer.py
@@ -0,0 +1,24 @@
+from abc import *
+
+from world_pop.population_data import PopulationData
+
+
+class PopulationDataObserver(ABC):
+    @abstractmethod
+    def on_change(self, stats: PopulationData):
+        pass
+
+
+class PopulationDataObservable:
+    def __init__(self):
+        self._observers = set()
+
+    def add_observer(self, observer: PopulationDataObserver):
+        self._observers.add(observer)
+
+    def remove_observer(self, observer: PopulationDataObserver):
+        self._observers.remove(observer)
+
+    def report_stats(self, data: PopulationData):
+        for o in self._observers:
+            o.on_change(data)
