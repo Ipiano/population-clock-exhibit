f104e8e1a88646b81c033224e5e0bfcbee62df92
Andrew Stelter <andrew.d.stelter@gmail.com>
2022-11-09 21:45:17 -0600

    Repeatedly try to ping the internet if no data is known
    This prevents having to wait a long time to get an initial value the first
    time the application runs on a system, if the network stack isn't
    quite ready right away.

--- app/cmdline.py
+++ app/cmdline.py
@@ -5,12 +5,15 @@ import logging
 import signal
 import time
 
+from population.population_data import PopulationData
 from population.population_calculator import PopulationCalculator
 from population.population_data_file import PopulationDataFile
 from population.population_data_updater import PopulationDataUpdater
+from population.population_data_observer import PopulationDataObserver
 from util.repeating_timer import RepeatingTimer
 from display.population_display import PopulationDisplay
 
+LOGGER = logging.getLogger("population")
 
 def parse_args():
     ap = ArgumentParser(
@@ -67,19 +70,50 @@ def main(args):
 
     data_file = PopulationDataFile(args.path)
     updater = PopulationDataUpdater()
+
+    LOGGER.info("Loading cached population stats...")
     provider = PopulationCalculator(data_file.load())
+
+    latest_stats = None
+    class LocalObserver(PopulationDataObserver):
+        def __init__(self):
+            pass
+
+        def on_change(self, data: PopulationData):
+            nonlocal latest_stats
+            latest_stats = data
+
+    local_observer = LocalObserver()
 
     updater.add_observer(provider)
     updater.add_observer(data_file)
-
-    # Initial update to get us started
-    updater.update()
+    updater.add_observer(local_observer)
 
     # Ping the site every so often
     update_timer = RepeatingTimer(timedelta(seconds=args.update), updater.update)
     update_timer.start()
 
+    # Regardless of what the regular ping time is, ping every few seconds until the
+    # first reading is available
+    def initial_updates():
+        nonlocal latest_stats
+
+        if latest_stats is None:
+            LOGGER.info("Attempting to get initial value...")
+            updater.update()
+
+        # Would be nice to stop the initial updates timer here
+        # but I can't figure out how to properly capture it inside
+        # this function to call stop() on it
+
+    initial_timer = RepeatingTimer(timedelta(seconds=5), initial_updates)
+    initial_timer.start()
+
+    initial_updates()
+
     if args.graphical or args.fullscreen:
+        LOGGER.info("Starting UI...")
+
         ui = PopulationDisplay(provider, timedelta(seconds=args.interval), fullscreen=args.fullscreen)
         ui.run()
     else:
@@ -109,6 +143,7 @@ def main(args):
         print_timer.stop()
 
     update_timer.stop()
+    initial_timer.stop()
 
 
 if __name__ == "__main__":
